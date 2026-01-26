0a326fd68384fb4c58a487d008cbe2d10e22c4e1
mojeed-painless <shittumjd00@gmail.com>
2026-01-26 13:23:21 +0100

    UPDATED

--- src/utils/quizHelper.js
+++ src/utils/quizHelper.js
@@ -15,7 +15,7 @@ export const getQuizWindowStatus = () => {
   const seconds = now.getSeconds();
 
   // 21:30:00 to 21:31:59 (9:30 PM to 9:31:59 PM)
-  const quizStartHour = 21;
+  const quizStartHour = 13;
   const quizStartMinute = 30;
   const quizEndMinute = 32;
 
